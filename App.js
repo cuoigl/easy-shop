01ee4623195c005abfaafd22e779411281d0354c
Cuoi <cuoi@Nguyens-MacBook-Pro.local>
2023-06-22 14:03:22 +0700

    Intro and Folder Structure

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+// Screens
+import ProductContainer from "./Screens/Products/ProductContainer";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <ProductContainer />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ Screens/Products/ProductContainer.js
@@ -0,0 +1,12 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+
+const ProductContainer = () => {
+  return (
+    <View>
+      <Text>Product Container</Text>
+    </View>
+  );
+};
+
+export default ProductContainer;
