ec251d1b56399d4cc5a911fbbcd2c4a78d65198b
Cuoi <cuoi@Nguyens-MacBook-Pro.local>
2023-06-22 14:14:13 +0700

    FlatList Products

--- Screens/Products/ProductContainer.js
+++ Screens/Products/ProductContainer.js
@@ -1,10 +1,35 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
+
+const data = require("../../assets/data/products.json");
 
 const ProductContainer = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(data);
+    return () => {
+      setProducts([]);
+    };
+  }, []);
   return (
     <View>
       <Text>Product Container</Text>
+      <View style={{ marginTop: 100 }}>
+        <FlatList
+          horizontal
+          data={products}
+          renderItem={({ item }) => <Text>{item.brand}</Text>}
+          keyExtractor={(item) => item.name}
+        ></FlatList>
+      </View>
     </View>
   );
 };
