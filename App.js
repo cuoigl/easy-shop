940482534625da7d6f2f6af0c61301b618077831
Cuoi <cuoi@Nguyens-MacBook-Pro.local>
2023-06-30 13:50:23 +0700

    Redux Store

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import "react-native-gesture-handler";
 import { StyleSheet, View, LogBox } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
+// Redux
+import { Provider } from "react-redux";
+import store from "./Redux/store";
+
 // Screens
 import Header from "./Screens/Shared/Header";
 import ProductContainer from "./Screens/Products/ProductContainer";
