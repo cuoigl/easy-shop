7fbff63618f667d95fd2700fe1c723e53504b692
Cuoi <cuoi@Nguyens-MacBook-Pro.local>
2023-06-30 13:40:20 +0700

    Single Product

--- Screens/Products/SingleProducts.js
+++ Screens/Products/SingleProducts.js
@@ -27,7 +27,31 @@ const SingleProduct = (props) => {
             style={styles.image}
           />
         </View>
+        <View style={styles.contentContainer}>
+          <H1 style={styles.contentHeader}>{item.name}</H1>
+          <Text style={styles.contentText}>{item.brand}</Text>
+        </View>
+        {/* <View style={styles.availabilityContainer}>
+          <View style={styles.availability}>
+            <Text style={{ marginRight: 10 }}>
+              Availability: {availabilityText}
+            </Text>
+            {availability}
+          </View>
+          <Text>{item.description}</Text>
+        </View> */}
       </ScrollView>
+
+      <View style={styles.bottomContainer}>
+        <Left>
+          <Text style={styles.price}>$ {item.price}</Text>
+        </Left>
+        <Right>
+          <Button title="Add">
+            <Text style={{ color: "white" }}>Add</Text>
+          </Button>
+        </Right>
+      </View>
     </Container>
   );
 };
@@ -45,6 +69,40 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     height: 250,
+  },
+  contentContainer: {
+    marginTop: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  contentHeader: {
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  contentText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  bottomContainer: {
+    flexDirection: "row",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    backgroundColor: "white",
+  },
+  price: {
+    fontSize: 24,
+    margin: 20,
+    color: "red",
+  },
+  availabilityContainer: {
+    marginBottom: 20,
+    alignItems: "center",
+  },
+  availability: {
+    flexDirection: "row",
+    marginBottom: 10,
   },
 });
 
