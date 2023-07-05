eea0241e8c1667f35f46600cccce933063bf4fda
Cuoi <cuoi@Nguyens-MacBook-Pro.local>
2023-07-05 10:18:17 +0700

    Cart Item

--- Screens/Cart/Cart.js
+++ Screens/Cart/Cart.js
@@ -1,14 +1,59 @@
-import { Text, View } from "react-native";
+import { View, Dimensions, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  Container,
+  Text,
+  Left,
+  Right,
+  H1,
+  ListItem,
+  Thumbnail,
+  Body,
+} from "native-base";
+
+import Icon from "react-native-vector-icons/FontAwesome";
 
 import { connect } from "react-redux";
+import * as actions from "../../Redux/Actions/cartActions";
+
+var { height, width } = Dimensions.get("window");
 
 const Cart = (props) => {
   return (
-    <View style={{ flex: 1 }}>
-      {props.cartItems.map((x) => {
-        return <Text>{x.product.name}</Text>;
-      })}
-    </View>
+    <>
+      {props.cartItems.length ? (
+        <Container>
+          <H1 style={{ alignSelf: "center" }}>Cart</H1>
+          {props.cartItems.map((data) => {
+            return (
+              <ListItem style={styles.listItem} key={Math.random()} avatar>
+                <Left>
+                  <Thumbnail
+                    source={{
+                      uri: data.product.image
+                        ? data.product.image
+                        : "https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png",
+                    }}
+                  />
+                </Left>
+                <Body style={styles.body}>
+                  <Left>
+                    <Text>{data.product.name}</Text>
+                  </Left>
+                  <Right>
+                    <Text>$ {data.product.price}</Text>
+                  </Right>
+                </Body>
+              </ListItem>
+            );
+          })}
+        </Container>
+      ) : (
+        <Container style={styles.emptyContainer}>
+          <Text>Looks like your cart is empty</Text>
+          <Text>Add product to your cart to get started</Text>
+        </Container>
+      )}
+    </>
   );
 };
 
@@ -18,5 +63,49 @@ const mapStateToProps = (state) => {
     cartItems: cartItems,
   };
 };
+
+const styles = StyleSheet.create({
+  emptyContainer: {
+    height: height,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  bottomContainer: {
+    flexDirection: "row",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    backgroundColor: "white",
+    elevation: 20,
+  },
+  price: {
+    fontSize: 18,
+    margin: 20,
+    color: "red",
+  },
+  hiddenContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    flexDirection: "row",
+  },
+  hiddenButton: {
+    backgroundColor: "red",
+    justifyContent: "center",
+    alignItems: "flex-end",
+    paddingRight: 25,
+    height: 70,
+    width: width / 1.2,
+  },
+  listItem: {
+    alignItems: "center",
+    backgroundColor: "white",
+    justifyContent: "center",
+  },
+  body: {
+    margin: 10,
+    alignItems: "center",
+    flexDirection: "row",
+  },
+});
 
 export default connect(mapStateToProps, null)(Cart);
