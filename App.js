e767a2c97a01fd5988a1846be432d40974faa82a
Cuoi <cuoi@Nguyens-MacBook-Pro.local>
2023-07-15 02:18:49 +0700

    Store

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
 import { LogBox } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Toast from "react-native-toast-message";
@@ -7,22 +8,27 @@ import Toast from "react-native-toast-message";
 import { Provider } from "react-redux";
 import store from "./Redux/store";
 
-// Screens
-import Header from "./Shared/Header";
+// Context API
+import Auth from "./Context/store/Auth";
 
 // Navigators
 import Main from "./Navigators/Main";
+
+// Screens
+import Header from "./Shared/Header";
 
 LogBox.ignoreAllLogs(true);
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Header />
-        <Main />
-        <Toast ref={(ref) => Toast.setRef(ref)} />
-      </NavigationContainer>
-    </Provider>
+    <Auth>
+      <Provider store={store}>
+        <NavigationContainer>
+          <Header />
+          <Main />
+          <Toast ref={(ref) => Toast.setRef(ref)} />
+        </NavigationContainer>
+      </Provider>
+    </Auth>
   );
 }
