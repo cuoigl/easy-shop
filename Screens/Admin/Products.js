52ba47b19f893d0b2119431248a95a33f9039a2f
Cuoi <cuoi@Nguyens-MacBook-Pro.local>
2023-07-17 05:38:00 +0700

    Product List Setup

--- Screens/Admin/Products.js
+++ Screens/Admin/Products.js
@@ -1,11 +1,87 @@
-import { View, Text } from "react-native";
+import React, { useState, useCallback } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  ActivityIndicator,
+  StyleSheet,
+  Dimensions,
+  Button,
+} from "react-native";
+import { Header, Item, Input } from "native-base";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { useFocusEffect } from "@react-navigation/native";
+import ListItem from "./ListItem";
+
+import axios from "axios";
+import baseURL from "../../assets/common/baseUrl";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+var { height, width } = Dimensions.get("window");
 
 const Products = (props) => {
+  const [productList, setProductList] = useState();
+  const [productFilter, setProductFilter] = useState();
+  const [loading, setLoading] = useState(true);
+  const [token, setToken] = useState();
+
+  useFocusEffect(
+    useCallback(() => {
+      // Get Token
+      AsyncStorage.getItem("jwt")
+        .then((res) => {
+          setToken(res);
+        })
+        .catch((error) => console.log(error));
+
+      axios.get(`${baseURL}products`).then((res) => {
+        setProductList(res.data);
+        setProductFilter(res.data);
+        setLoading(false);
+      });
+
+      return () => {
+        setProductList();
+        setProductFilter();
+        setLoading(true);
+      };
+    }, [])
+  );
   return (
     <View>
       <Text>Products Screen</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  listHeader: {
+    flexDirection: "row",
+    padding: 5,
+    backgroundColor: "gainsboro",
+  },
+  headerItem: {
+    margin: 3,
+    width: width / 6,
+  },
+  spinner: {
+    height: height / 2,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container: {
+    marginBottom: 160,
+    backgroundColor: "white",
+  },
+  buttonContainer: {
+    margin: 20,
+    alignSelf: "center",
+    flexDirection: "row",
+  },
+  buttonText: {
+    marginLeft: 4,
+    color: "white",
+  },
+});
 
 export default Products;
